1b1673d1da688af241840938eb3b6d4134fd0f26
SAZOL <mhsazol111@gmail.com>
2018-04-30 20:51:49 +0600

    no message

--- app/Task.php
+++ app/Task.php
@@ -5,6 +5,9 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
-{
-    //
+{	
+	public static function incomplete() 
+	{
+		return static::where( 'completed', 0 )->get();
+	}
 }
